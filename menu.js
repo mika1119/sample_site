7b45991a90e3fae972970298b16a208251f864a3
mika1119 <mika1119.work@gmail.com>
2022-03-26 17:12:31 +0900

    Finish nav menu.js

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ $(function() {
   $('.header-btn').on('click', function() {
     $('.header-btn__bar1, .header-btn__bar2').toggleClass('open');
     $('.header-btn__text').toggleClass('active');
+    $(".is-open").slideToggle();
   })
 });
 
