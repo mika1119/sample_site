433f9357c41ae771a133880794182a50aa475c0c
mika1119 <mika1119.work@gmail.com>
2022-06-15 10:47:16 +0900

    Fix jQuery to javascript

--- menu.js
+++ menu.js
@@ -1,9 +1,24 @@
-$(function() {
-  $('.header-btn').on('click', function() {
-    $('.header-btn__bar1, .header-btn__bar2').toggleClass('open');
-    $('.header-btn__text').toggleClass('active');
-    $(".is-open").slideToggle();
-  })
-});
+// $(function() {
+//   $('.header-btn').on('click', function() {
+//     $('.header-btn__bar1, .header-btn__bar2').toggleClass('open');
+//     $('.header-btn__text').toggleClass('active');
+//     $(".is-open").slideToggle();
+//   })
+// });
 
 console.log('hello');
+
+let btn = document.getElementById('header-btn');
+let nav = document.getElementById('nav');
+
+let text = document.getElementById('header-btn__text'); 
+
+let bar1 = document.getElementById('header-btn__bar1');
+let bar2 = document.getElementById('header-btn__bar2');
+
+btn.addEventListener('click', e => {
+  text.classList.toggle('active');
+  bar1.classList.toggle('open');
+  bar2.classList.toggle('open');
+  nav.classList.toggle('is-open');
+})
