79a5715b2f625b39f79bea83eb0b3306445a3d5d
show kawazoe <show753@icloud.com>
2022-06-15 11:18:29 +0900

    トグルメニューのリファクタリング

--- menu.js
+++ menu.js
@@ -11,14 +11,15 @@ console.log('hello');
 let btn = document.getElementById('header-btn');
 let nav = document.getElementById('nav');
 
-let text = document.getElementById('header-btn__text'); 
+// let text = document.getElementById('header-btn__text');
 
-let bar1 = document.getElementById('header-btn__bar1');
-let bar2 = document.getElementById('header-btn__bar2');
+// let bar1 = document.getElementById('header-btn__bar1');
+// let bar2 = document.getElementById('header-btn__bar2');
 
 btn.addEventListener('click', e => {
-  text.classList.toggle('active');
-  bar1.classList.toggle('open');
-  bar2.classList.toggle('open');
+  // text.classList.toggle('active');
+  // bar1.classList.toggle('open');
+  // bar2.classList.toggle('open');
+  btn.classList.toggle('is-open');
   nav.classList.toggle('is-open');
 })
